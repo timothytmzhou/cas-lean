ee7635ee828a19ac010e40a9c2b970efc43e0151
timothytmzhou <timothytmzhou@gmail.com>
2023-12-21 14:25:44 -0800

    structure for sage calls in lean

--- cas/Cas/main.lean
+++ cas/Cas/main.lean
@@ -9,3 +9,18 @@ inductive Cmd :=
 def Spec (cmd : Cmd) : Prop := match cmd with
   | Cmd.roots p => ∃ a : algebraic, a.root_of = p
 
+def callSage (query : String) : IO String := sorry
+
+def findRoot (p : poly) : IO algebraic := do
+  let serialized := toString p
+  let query := sorry
+  let res ← callSage query
+  let parse_algebraic (s : String) : algebraic := sorry
+  pure $ parse_algebraic res
+
+def runCmd (cmd : Cmd) : MetaM Unit :=
+  let spec := Spec cmd
+  match cmd with
+  | Cmd.roots p => do
+    let r ← findRoot p
+    sorry
